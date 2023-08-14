f3d905247f7d087c285347fe2448eec6999f90ed
GamithaGimhana <gamitha.gimhana99@gmail.com>
2023-08-14 16:40:56 +0530

    first commit

---
+++ src/supermarket/layerd/SupermarketLayerd.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarket.layerd;
+
+import supermarket.layerd.view.LayoutView;
+/**
+ *
+ * @author Nandarathana
+ */
+public class SupermarketLayerd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         new LayoutView().setVisible(true);// TODO code application logic here
+    }
+    
+}
---
+++ src/supermarket/layerd/dao/custom/CustomerDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package supermarket.layerd.dao.custom;
+
+import supermarket.layerd.dao.CrudDao;
+import supermarket.layerd.entity.CustomerEntity;
+/**
+ *
+ * @author Nandarathana
+ */
+public interface CustomerDao extends CrudDao<CustomerEntity, String>{
+    
+}
